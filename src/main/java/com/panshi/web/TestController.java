532ce5e8cf5a928cec10919438cbbf3885747a01
zhanglaoban123456 <2967192670@qq.com>
2021-06-19 15:45:44 +0800

    蒋涛

---
+++ src/main/java/com/panshi/web/TestController.java
@@ -0,0 +1,16 @@
+package com.panshi.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/student")
+public class TestController {
+
+    @RequestMapping("/test")
+    public  String test(){
+        System.out.println("进来了");
+        return "test.jsp";
+    }
+}
