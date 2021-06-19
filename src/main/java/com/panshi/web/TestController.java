8a3544ca9c0d4b967d53922867838470cce69c00
zhanglaoban123456 <2967192670@qq.com>
2021-06-19 16:29:48 +0800

    蒋涛第二次

--- src/main/java/com/panshi/web/TestController.java
+++ src/main/java/com/panshi/web/TestController.java
@@ -2,21 +2,15 @@ package com.panshi.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/jsp")
 public class TestController {
-    /*
-    *  /student/test
-    *  /student/
-    *  /test.jsp
-    *
-    * */
 
     @RequestMapping("/test")
     public  String test(){
-        System.out.println("进不来了aaa");
-        return "../test.jsp";
+        System.out.println("进来了");
+        return "test.jsp";
     }
 }
